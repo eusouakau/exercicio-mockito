7f533f3e7705f1f4b75914d6765c287cf1a7913e
Kauana Henriques <karoltvbl@gmail.com>
2023-04-25 01:15:41 -0300

    instancia audioManager

--- src/main/java/ifrs/dev/VolumeUtil.java
+++ src/main/java/ifrs/dev/VolumeUtil.java
@@ -2,7 +2,7 @@ package ifrs.dev;
 
 public class VolumeUtil {
 
-    private AudioManager audioManager;
+    private AudioManager audioManager = new AudioManager();
 
     public void maximizeVolume(int max) {
         audioManager.setVolume(max);
