4f58bbee238eb6059732ebe863e2843c672c33a6
Kauana Henriques <karoltvbl@gmail.com>
2023-04-25 01:15:23 -0300

    corrige importação

--- src/test/java/ifrs/dev/VolumeUtilTest.java
+++ src/test/java/ifrs/dev/VolumeUtilTest.java
@@ -2,18 +2,18 @@ package ifrs.dev;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 @ExtendWith(MockitoExtension.class)
 public class VolumeUtilTest {
 
     @Mock
-    AudioManager audioManager = new AudioManager();
+    AudioManager audioManager;
 
     @InjectMocks
     VolumeUtil volumeUtil = new VolumeUtil();
@@ -21,7 +21,7 @@ public class VolumeUtilTest {
 
     @Test
     public void testAudioManagerSetVolume() {
-
+        
         volumeUtil.maximizeVolume(1);
  
         verify(audioManager, times(1)).setVolume(1);
